ce481537ae2965e945c9eaf4180041d0b37687ff
barto <18618583+rajlu@users.noreply.github.com>
2023-05-07 12:12:50 +0200

    fix: add "forwardRef" to "TextField component" and fix full width

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -8,23 +8,31 @@ type Props = {
   trailingIcon?: JSX.Element;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-function TextField({ helperText, ...props }: Props) {
-  return (
-    <div className="space-y-1">
-      {props.label && <label className="text-p-small">{props.label}</label>}
-      <div className="overflow-auto rounded-2 border border-green-900">
-        <span className="icon leading-icon">{props.leadingIcon}</span>
-        <input className="h-[48px] w-full rounded-2 px-4" {...props} />
-        <span className="icon trailing-icon">{props.trailingIcon}</span>
-      </div>
-      {helperText && (
-        <div className="flex gap-1">
-          <IconInfoCircle size={20} />
-          <small className="text-p-small">{helperText}</small>
+const TextField = React.forwardRef<HTMLInputElement, Props>(
+  ({ helperText, ...props }, ref) => {
+    return (
+      <div className="w-full space-y-1">
+        {props.label && <label className="text-p-small">{props.label}</label>}
+        <div className="overflow-auto rounded-2 border border-green-900">
+          <span className="icon leading-icon">{props.leadingIcon}</span>
+          <input
+            ref={ref}
+            className="h-[48px] w-full rounded-2 px-4"
+            {...props}
+          />
+          <span className="icon trailing-icon">{props.trailingIcon}</span>
         </div>
-      )}
-    </div>
-  );
-}
+        {helperText && (
+          <div className="flex gap-1">
+            <IconInfoCircle size={20} />
+            <small className="text-p-small">{helperText}</small>
+          </div>
+        )}
+      </div>
+    );
+  }
+);
+
+TextField.displayName = 'TextField';
 
 export default TextField;
