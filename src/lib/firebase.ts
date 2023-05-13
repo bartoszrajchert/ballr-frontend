541e3804554c895fae625d6be2dfb9220c756b89
barto <18618583+rajlu@users.noreply.github.com>
2023-05-13 23:52:52 +0200

    Revert "fix: prevent additional initialization of the firebase app on hot reloads"
    This reverts commit d29e08798d53f62bd428d0bbde818962a65db91e.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApps } from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyD-CcdWcmy5O-D_Pa1rhiCnHr99GQXH9_U',
@@ -9,6 +9,6 @@ const firebaseConfig = {
   appId: '1:828798100898:web:52b6b9a5fee9717cffbefb',
 };
 
-if (typeof window !== 'undefined' && !getApps().length) {
-  initializeApp(firebaseConfig);
-}
+const app = initializeApp(firebaseConfig);
+
+export default app;
