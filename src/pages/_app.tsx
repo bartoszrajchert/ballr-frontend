579a069da694fb03e761a6838f026f38bb53071b
barto <18618583+rajlu@users.noreply.github.com>
2023-05-22 01:09:07 +0200

    fix: navigation rerender

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,31 @@
+import Footer from '@/components/Footer';
+import Navigation from '@/components/Navigation';
 import VerifyEmailBanner from '@/components/VerifyEmailBanner';
 import '@/i18n/config';
 import '@/lib/firebase';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+const focusModePaths = ['/login', '/register', '/forgot-password'];
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const focusMode = focusModePaths.includes(router.pathname);
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <VerifyEmailBanner />
+      <Navigation focusMode={focusMode} />
       <Component {...pageProps} />
+      {!focusMode && <Footer />}
       <ToastContainer
         position="bottom-right"
         autoClose={5000}
