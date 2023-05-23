dfdb52f15302bde35aef09ca234d21766a7a51b9
barto <18618583+rajlu@users.noreply.github.com>
2023-05-23 23:38:38 +0200

    refactor: move firebase config to env variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD-CcdWcmy5O-D_Pa1rhiCnHr99GQXH9_U',
-  authDomain: 'ballr-ec1d8.firebaseapp.com',
-  projectId: 'ballr-ec1d8',
-  storageBucket: 'ballr-ec1d8.appspot.com',
-  messagingSenderId: '828798100898',
-  appId: '1:828798100898:web:52b6b9a5fee9717cffbefb',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLI_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLI_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
