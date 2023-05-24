4ed7e5fdfa610d06c16e994840d6ef5af3cd58cd
barto <18618583+rajlu@users.noreply.github.com>
2023-05-24 21:41:30 +0200

    feat: add "nookies", "nprogress", "query-string", backend fetcher, add middleware for protected pages

--- src/lib/fetchers.ts
+++ src/lib/fetchers.ts
@@ -1,5 +1,18 @@
 import axios from 'axios';
+import { GetServerSidePropsContext } from 'next';
+import nookies from 'nookies';
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
-export { fetcher };
+const fetcherBackend = (url: string, context: GetServerSidePropsContext) => {
+  const token = nookies.get(context).token;
+
+  return axios
+    .get(url, { headers: { Authorization: `Bearer ${token}` } })
+    .then((res) => res.data)
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export { fetcher, fetcherBackend };
