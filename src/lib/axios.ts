0417c4bca36d7925e16cb227a5638929a065ca90
barto <18618583+rajlu@users.noreply.github.com>
2023-05-24 21:39:00 +0200

    fix: axios token interceptor

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,18 +1,34 @@
-import { getApp } from '@firebase/app';
 import axios, { InternalAxiosRequestConfig } from 'axios';
-import { getAuth } from 'firebase/auth';
+import { getAuth, getIdToken, onAuthStateChanged } from 'firebase/auth';
 
 export default function initAxios() {
   axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
-  axios.interceptors.request.use(tokenInterceptor);
+  axios.interceptors.request.use(tokenInterceptor, (error) =>
+    Promise.reject(error)
+  );
 }
 
 async function tokenInterceptor(config: InternalAxiosRequestConfig) {
-  const auth = getAuth(getApp());
-  const token = await auth.currentUser?.getIdToken();
+  const token = await getUserToken();
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
 }
+
+const getUserToken = async () => {
+  return new Promise((resolve, reject) => {
+    const unsub = onAuthStateChanged(getAuth(), async (user) => {
+      if (user) {
+        const token = await getIdToken(user);
+        resolve(token);
+      } else {
+        console.log('User not logged in');
+        resolve(null);
+      }
+      unsub();
+    });
+  });
+};
