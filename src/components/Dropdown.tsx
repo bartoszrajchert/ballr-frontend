4e178a8ffa388686c0748063295229b2562cf898
barto <18618583+rajlu@users.noreply.github.com>
2023-05-24 21:44:14 +0200

    feat: update dropdown component and matches page

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -5,9 +5,11 @@ import React from 'react';
 import { Control, Controller } from 'react-hook-form';
 
 type Props = {
-  label?: string;
   name: string;
   control: Control;
+  data: { label: string; value: string }[];
+  label?: string;
+  resetField?: () => void;
 };
 
 const Dropdown = (props: Props) => {
@@ -18,47 +20,48 @@ const Dropdown = (props: Props) => {
         control={props.control}
         name={props.name}
         render={({ field }) => (
-          <Select.Root value={field.value} onValueChange={field.onChange}>
-            <Select.Trigger
-              id={props.name}
-              className="flex h-[48px] w-full appearance-none items-center justify-between rounded-2 px-4 shadow-border-1px outline-none transition-shadow hover:shadow-border-2px focus:shadow-border-3px focus-visible:shadow-border-3px"
-              aria-label="Food"
+          <div className="flex gap-1">
+            <Select.Root
+              value={field.value}
+              onValueChange={field.onChange}
+              name={field.name}
             >
-              <Select.Value />
-              <Select.Icon>
-                <IconChevronDown />
-              </Select.Icon>
-            </Select.Trigger>
-            <Select.Portal>
-              <Select.Content className="w-full rounded-md border border-green-900 bg-white">
-                <Select.ScrollUpButton className="m-1 flex h-[25px] cursor-default items-center justify-center bg-white">
-                  <IconChevronUp />
-                </Select.ScrollUpButton>
-                <Select.Viewport className="p-1">
-                  <SelectItem value="apple">Apple</SelectItem>
-                  <SelectItem value="banana">Banana</SelectItem>
-                  <SelectItem value="blueberry">Blueberry</SelectItem>
-                  <SelectItem value="grapes">Grapes</SelectItem>
-                  <SelectItem value="pineapple">Pineapple</SelectItem>
-                  <SelectItem value="strawberry">Strawberry</SelectItem>
-                  <SelectItem value="watermelon">Watermelon</SelectItem>
-                  <SelectItem value="orange">Orange</SelectItem>
-                  <SelectItem value="pear">Pear</SelectItem>
-                  <SelectItem value="cherry">Cherry</SelectItem>
-                  <SelectItem value="mango">Mango</SelectItem>
-                  <SelectItem value="lemon">Lemon</SelectItem>
-                  <SelectItem value="kiwi">Kiwi</SelectItem>
-                  <SelectItem value="peach">Peach</SelectItem>
-                  <SelectItem value="avocado">Avocado</SelectItem>
-                  <SelectItem value="plum">Plum</SelectItem>
-                  <SelectItem value="fig">Fig</SelectItem>
-                </Select.Viewport>
-                <Select.ScrollDownButton className="m-1 flex h-[25px] cursor-default items-center justify-center bg-white">
+              <Select.Trigger
+                id={props.name}
+                className="flex h-[48px] w-full appearance-none items-center justify-between rounded-2 px-4 shadow-border-1px outline-none transition-shadow hover:shadow-border-2px focus:shadow-border-3px focus-visible:shadow-border-3px"
+              >
+                <Select.Value asChild>
+                  <span>
+                    {
+                      props.data.find((data) => data.value === field.value)
+                        ?.label
+                    }
+                  </span>
+                </Select.Value>
+
+                <Select.Icon>
                   <IconChevronDown />
-                </Select.ScrollDownButton>
-              </Select.Content>
-            </Select.Portal>
-          </Select.Root>
+                </Select.Icon>
+              </Select.Trigger>
+              <Select.Portal>
+                <Select.Content className="w-full rounded-md border border-green-900 bg-white">
+                  <Select.ScrollUpButton className="m-1 flex h-[25px] cursor-default items-center justify-center bg-white">
+                    <IconChevronUp />
+                  </Select.ScrollUpButton>
+                  <Select.Viewport className="p-1">
+                    {props.data.map((item) => (
+                      <SelectItem key={item.value} value={item.value}>
+                        {item.label}
+                      </SelectItem>
+                    ))}
+                  </Select.Viewport>
+                  <Select.ScrollDownButton className="m-1 flex h-[25px] cursor-default items-center justify-center bg-white">
+                    <IconChevronDown />
+                  </Select.ScrollDownButton>
+                </Select.Content>
+              </Select.Portal>
+            </Select.Root>
+          </div>
         )}
       />
     </div>
