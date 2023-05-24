3be5cad92c519f2446637827410328ade5af29ea
barto <18618583+rajlu@users.noreply.github.com>
2023-05-24 14:20:15 +0200

    feat: add simple dropdown and implement it in "matches" page

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,88 @@
+import Label from '@/components/Label';
+import * as Select from '@radix-ui/react-select';
+import { IconCheck, IconChevronDown, IconChevronUp } from '@tabler/icons-react';
+import React from 'react';
+import { Control, Controller } from 'react-hook-form';
+
+type Props = {
+  label?: string;
+  name: string;
+  control: Control;
+};
+
+const Dropdown = (props: Props) => {
+  return (
+    <div className="w-full space-y-1">
+      {props.label && <Label value={props.label} htmlFor={props.name} />}
+      <Controller
+        control={props.control}
+        name={props.name}
+        render={({ field }) => (
+          <Select.Root value={field.value} onValueChange={field.onChange}>
+            <Select.Trigger
+              id={props.name}
+              className="flex h-[48px] w-full appearance-none items-center justify-between rounded-2 px-4 shadow-border-1px outline-none transition-shadow hover:shadow-border-2px focus:shadow-border-3px focus-visible:shadow-border-3px"
+              aria-label="Food"
+            >
+              <Select.Value />
+              <Select.Icon>
+                <IconChevronDown />
+              </Select.Icon>
+            </Select.Trigger>
+            <Select.Portal>
+              <Select.Content className="w-full rounded-md border border-green-900 bg-white">
+                <Select.ScrollUpButton className="m-1 flex h-[25px] cursor-default items-center justify-center bg-white">
+                  <IconChevronUp />
+                </Select.ScrollUpButton>
+                <Select.Viewport className="p-1">
+                  <SelectItem value="apple">Apple</SelectItem>
+                  <SelectItem value="banana">Banana</SelectItem>
+                  <SelectItem value="blueberry">Blueberry</SelectItem>
+                  <SelectItem value="grapes">Grapes</SelectItem>
+                  <SelectItem value="pineapple">Pineapple</SelectItem>
+                  <SelectItem value="strawberry">Strawberry</SelectItem>
+                  <SelectItem value="watermelon">Watermelon</SelectItem>
+                  <SelectItem value="orange">Orange</SelectItem>
+                  <SelectItem value="pear">Pear</SelectItem>
+                  <SelectItem value="cherry">Cherry</SelectItem>
+                  <SelectItem value="mango">Mango</SelectItem>
+                  <SelectItem value="lemon">Lemon</SelectItem>
+                  <SelectItem value="kiwi">Kiwi</SelectItem>
+                  <SelectItem value="peach">Peach</SelectItem>
+                  <SelectItem value="avocado">Avocado</SelectItem>
+                  <SelectItem value="plum">Plum</SelectItem>
+                  <SelectItem value="fig">Fig</SelectItem>
+                </Select.Viewport>
+                <Select.ScrollDownButton className="m-1 flex h-[25px] cursor-default items-center justify-center bg-white">
+                  <IconChevronDown />
+                </Select.ScrollDownButton>
+              </Select.Content>
+            </Select.Portal>
+          </Select.Root>
+        )}
+      />
+    </div>
+  );
+};
+
+const SelectItem = React.forwardRef<
+  React.ElementRef<typeof Select.Item>,
+  React.ComponentPropsWithoutRef<typeof Select.Item>
+>((props, ref) => {
+  return (
+    <Select.Item
+      className="relative flex h-[25px] select-none items-center rounded-[3px] pl-[25px] pr-[35px] data-[disabled]:pointer-events-none data-[highlighted]:bg-green-900 data-[highlighted]:text-white data-[highlighted]:outline-none"
+      ref={ref}
+      {...props}
+    >
+      <Select.ItemText>{props.children}</Select.ItemText>
+      <Select.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
+        <IconCheck size={16} />
+      </Select.ItemIndicator>
+    </Select.Item>
+  );
+});
+
+SelectItem.displayName = 'SelectItem';
+
+export default Dropdown;
