28b2654ae1967974d32c3738a5bb2c2ff56f893c
barto <18618583+rajlu@users.noreply.github.com>
2023-05-29 20:07:45 +0200

    fix: avatar size

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -13,21 +13,23 @@ const Avatar = (props: Props) => {
   const size = props.size ? props.size : 48;
 
   return (
-    <div
-      className={clsx(
-        'flex items-center justify-center rounded-full bg-green-200',
-        {
-          'cursor-pointer shadow-border-1px shadow-green-400 transition-colors hover:bg-green-300':
-            props.clickable,
-        },
-        props.className
-      )}
-      style={{
-        height: size,
-        width: size,
-      }}
-    >
-      <p className="text-label-medium text-green-900">{initials}</p>
+    <div>
+      <div
+        className={clsx(
+          'flex items-center justify-center rounded-full bg-green-200',
+          {
+            'cursor-pointer shadow-border-1px shadow-green-400 transition-colors hover:bg-green-300':
+              props.clickable,
+          },
+          props.className
+        )}
+        style={{
+          height: size,
+          width: size,
+        }}
+      >
+        <p className="text-label-medium text-green-900">{initials}</p>
+      </div>
     </div>
   );
 };
