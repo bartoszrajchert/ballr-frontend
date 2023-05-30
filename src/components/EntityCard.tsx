3d2c43a60909e9747e3c37842e2d48d6985eeb0f
barto <18618583+rajlu@users.noreply.github.com>
2023-05-30 19:33:39 +0200

    feat: create EntityCard

---
+++ src/components/EntityCard.tsx
@@ -0,0 +1,24 @@
+import Avatar, { AvatarProps } from '@/components/Avatar';
+import Link from 'next/link';
+import React from 'react';
+
+type Props = {
+  href: string;
+  title: string;
+  avatar?: AvatarProps;
+  paragraph?: string;
+};
+
+function EntityCard(props: Props) {
+  return (
+    <Link href={props.href}>
+      <div className="flex flex-col items-center justify-center rounded-2xl bg-grey-100 p-7 shadow-border-1px shadow-grey-300 hover:bg-green-100">
+        {props.avatar && <Avatar {...props.avatar} className="mb-4" />}
+        <p className="text-label-medium">{props.title}</p>
+        <p>{props.paragraph}</p>
+      </div>
+    </Link>
+  );
+}
+
+export default EntityCard;
