6fc8825ec96f3624d377bc6d3aaba640e37a2001
barto <18618583+rajlu@users.noreply.github.com>
2023-06-02 18:12:32 +0200

    feat: add edit page

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,15 +1,19 @@
 import { BACKEND_ROUTES } from '@/lib/routes';
 import axios from 'axios';
 
-export type CreateUserData = {
+export type CreateUpdateUserData = {
   first_name: string;
   last_name: string;
-  birth_date: string;
+  birth_date: string; // Backend doesn't have update for this field
   notification_preference: boolean;
   gender_id: number;
   city_id: number;
 };
 
-export async function createUser(user: CreateUserData) {
+export async function createUser(user: CreateUpdateUserData) {
   return axios.post(`${BACKEND_ROUTES.USERS}`, user);
+}
+
+export async function updateUser(user: CreateUpdateUserData, id: string) {
+  return axios.put(`${BACKEND_ROUTES.USERS}/${id}`, user);
 }
