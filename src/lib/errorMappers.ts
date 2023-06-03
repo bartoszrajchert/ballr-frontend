639fcd0f0fe62e13c03967dd847a008cdc2567d4
barto <18618583+rajlu@users.noreply.github.com>
2023-06-04 01:10:01 +0200

    fix: add firebase error mapper and use it in security page

---
+++ src/lib/errorMappers.ts
@@ -0,0 +1,5 @@
+export function firebaseError(error: Error) {
+  if (error.message.includes('auth/requires-recent-login')) {
+    return 'Aby wykonać tę akcję, musisz się zalogować ponownie.';
+  }
+}
