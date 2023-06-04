6c881c21ab2984758f9e3b04e30147275f54b1eb
barto <18618583+rajlu@users.noreply.github.com>
2023-06-04 16:08:07 +0200

    feat: add "actionChildren" to EntityCard

--- src/components/EntityCard.tsx
+++ src/components/EntityCard.tsx
@@ -1,4 +1,5 @@
 import Avatar, { AvatarProps } from '@/components/Avatar';
+import clsx from 'clsx';
 import Link from 'next/link';
 import React from 'react';
 
@@ -9,20 +10,26 @@ type Props = {
   paragraph?: string;
   className?: string;
   leadingIcon?: JSX.Element;
+  actionChildren?: JSX.Element;
 };
 
 function EntityCard(props: Props) {
   return (
-    <Link href={props.href} className={props.className}>
-      <div className="flex h-full w-full flex-col items-center justify-center rounded-2xl bg-grey-100 p-7 shadow-border-1px shadow-grey-300 hover:bg-green-100">
-        {props.avatar && <Avatar {...props.avatar} className="mb-4" />}
-        <p className="flex items-center justify-center gap-1 text-label-medium">
-          {props.leadingIcon}
-          {props.title}
-        </p>
-        <p>{props.paragraph}</p>
-      </div>
-    </Link>
+    <div className={clsx(props.className, 'relative')}>
+      <Link href={props.href} className="h-full w-full">
+        <div className="flex h-full w-full flex-col items-center justify-center rounded-2xl bg-grey-100 p-7 shadow-border-1px shadow-grey-300 hover:bg-green-100">
+          {props.avatar && <Avatar {...props.avatar} className="mb-4" />}
+          <p className="flex items-center justify-center gap-1 text-label-medium">
+            {props.leadingIcon}
+            {props.title}
+          </p>
+          <p>{props.paragraph}</p>
+        </div>
+      </Link>
+      {props.actionChildren && (
+        <div className="absolute right-0.5 top-0.5">{props.actionChildren}</div>
+      )}
+    </div>
   );
 }
 
