4886ef88af105b12950e82aa0de6fcb95f18ee56
barto <18618583+rajlu@users.noreply.github.com>
2023-06-04 21:48:10 +0200

    feat: add skeleton on component loading

--- src/components/dynamic/DynamicDropdown.tsx
+++ src/components/dynamic/DynamicDropdown.tsx
@@ -1,3 +1,6 @@
 import dynamic from 'next/dynamic';
+import Skeleton from 'react-loading-skeleton';
 
-export const DynamicDropdown = dynamic(() => import('@/components/Dropdown'));
+export const DynamicDropdown = dynamic(() => import('@/components/Dropdown'), {
+  loading: () => <Skeleton height={40} />,
+});
