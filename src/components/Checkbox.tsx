d26381734d4866ca02f0311102962e4743a6400c
barto <18618583+rajlu@users.noreply.github.com>
2023-06-04 22:29:13 +0200

    feat: add disabled state to checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,6 +1,7 @@
 import Label from '@/components/Label';
 import * as RadixCheckbox from '@radix-ui/react-checkbox';
 import { IconCheck } from '@tabler/icons-react';
+import clsx from 'clsx';
 import React from 'react';
 import { Control, Controller } from 'react-hook-form';
 
@@ -8,6 +9,7 @@ type Props = {
   label: string;
   name: string;
   control: Control<any>;
+  disabled?: boolean;
 };
 
 const Checkbox = (props: Props) => {
@@ -16,15 +18,31 @@ const Checkbox = (props: Props) => {
       control={props.control}
       name={props.name}
       render={({ field }) => (
-        <div className="flex items-center gap-2">
+        <div
+          className={clsx('flex items-center gap-2', {
+            '!text-grey-400': props.disabled,
+          })}
+        >
           <RadixCheckbox.Root
             {...field}
             checked={String(field.value) === 'true'}
             onCheckedChange={field.onChange}
             id={props.name}
-            className=" flex h-[24px] w-[24px] appearance-none items-center justify-center rounded-[4px] bg-white shadow-border-2px !shadow-green-900 outline-none transition-shadow hover:shadow-border-3px"
+            className={clsx(
+              'flex h-[24px] w-[24px] appearance-none items-center justify-center rounded-[4px]',
+              'bg-white shadow-border-2px !shadow-green-900 outline-none',
+              'transition-shadow hover:shadow-border-3px',
+              {
+                '!shadow-grey-400 hover:!shadow-border-2px': props.disabled,
+              }
+            )}
+            disabled={props.disabled}
           >
-            <RadixCheckbox.Indicator className="bg-green-900 text-white">
+            <RadixCheckbox.Indicator
+              className={clsx('bg-green-900 text-white', {
+                'bg-grey-400': props.disabled,
+              })}
+            >
               <IconCheck size={20} />
             </RadixCheckbox.Indicator>
           </RadixCheckbox.Root>
