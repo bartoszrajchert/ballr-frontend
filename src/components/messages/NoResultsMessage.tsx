ae0ce083d87dc9bc2cc6fa81b63ee045f76a473e
barto <18618583+rajlu@users.noreply.github.com>
2023-06-06 00:32:23 +0200

    fix: show button when match is assigned to reservation, handle error and loading state

--- src/components/messages/NoResultsMessage.tsx
+++ src/components/messages/NoResultsMessage.tsx
@@ -1,10 +1,16 @@
 import { IconSearchOff } from '@tabler/icons-react';
 
-function NoResultsMessage() {
+type Props = {
+  message?: string;
+};
+
+function NoResultsMessage({ message }: Props) {
   return (
     <div className="flex w-full items-center justify-center gap-2 rounded-xl bg-grey-100 p-4 text-gray-400">
       <IconSearchOff className="h-[28px] w-[28px] sm:h-[48px] sm:w-[48px]" />
-      <h3 className="text-heading-h4 sm:text-heading-h3">Brak wyników</h3>
+      <h3 className="text-heading-h4 sm:text-heading-h3">
+        {message ?? 'Brak wyników'}
+      </h3>
     </div>
   );
 }
