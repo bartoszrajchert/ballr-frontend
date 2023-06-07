a3ed67136882a0e4418d4ce142a9b2ee7fed1931
barto <18618583+rajlu@users.noreply.github.com>
2023-06-07 17:02:03 +0200

    fix: change text colour for better contrast

--- src/components/messages/NoResultsMessage.tsx
+++ src/components/messages/NoResultsMessage.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function NoResultsMessage({ message }: Props) {
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-2 rounded-xl bg-grey-100 p-4 text-gray-400 sm:flex-row">
+    <div className="flex w-full flex-col items-center justify-center gap-2 rounded-xl bg-grey-100 p-4 text-gray-500 sm:flex-row">
       <IconSearchOff className="h-[28px] w-[28px] sm:h-[48px] sm:w-[48px]" />
       <h3 className="text-center text-heading-h5 sm:text-heading-h3">
         {message ?? 'Brak wyników'}
